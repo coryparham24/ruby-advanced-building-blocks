5ace012cf7dbade61e6d9a4f98ff037656519a08
Cory Parham <cory_parham24@hotmail.com>
2017-08-20 00:23:54 -0400

    Added bubble sort by method

--- project1.rb
+++ project1.rb
@@ -17,3 +17,25 @@ def bubble_sort(array)
 end
 
 bubble_sort([7, 84, 32, 97, 1])
+
+def bubble_sort_by(array)
+	for iteration in 1..array.length
+		for index in 0...array.length
+			if array[index] == array.last
+				index = 0
+			end
+
+			result = yield(array[index], array[index + 1])
+			if result > 0
+				array[index], array[index + 1] = array[index + 1], array[index]
+			end
+			index += 1
+		end
+		iteration += 1
+	end
+	puts array
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+	left.length - right.length
+end
